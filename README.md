b99a0e6551d3308b3ae4c0d14de8139e8523f2a9
Rodolfo Yabut <rodolfoyabut@gmail.com>
2016-07-24 09:26:42 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Component Logger
+Module that wraps a React component and sends props to Elasticsearch for querying
+
+# Installation
+`npm install component-logger`
---
+++ src/component-logger.js
@@ -0,0 +1,27 @@
+const elasticsearch = require('elasticsearch')
+
+class ComponentLogger {
+  constructor(host) {
+    this.connection = ((host = 'localhost:9200') => {
+      return new elasticsearch.Client({
+        host: host
+      })
+    })(host)
+  }
+
+  sendProps(componentName, props) {
+    this.connection.create({
+      index: 'component_logger',
+      type: 'props',
+      body: {
+        "name": componentName,
+        "props": props,
+        "created": Date.now()
+      }
+    })
+  }
+}
+
+module.exports = host => {
+  return new ComponentLogger(host)
+}
